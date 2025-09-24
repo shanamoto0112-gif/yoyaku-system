ed94f71578b2428624f7f4c1f1e74205b4c4edd0
shanamoto0112-gif <s.hanamoto0112@gmail.com>
2025-09-24 14:02:19 +0900

    firebase-config.js を追加

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDsgnJTAbc7FShXFXWBsR25IDXQox8sQjM ",
+    authDomain: "maruni-yoyaku-system.firebaseapp.com",
+    projectId: "maruni-yoyaku-system",
+    storageBucket: "maruni-yoyaku-system.firebasestorage.app",
+    messagingSenderId: "419757490787",
+    appId: "1:419757490787:web:095eb61e85d008ea2b2e56"
+};
